eb8af2c9845e057e4e44e95b7c8fda71afee6892
gaurav101203 <162018942+gaurav101203@users.noreply.github.com>
2025-03-28 00:18:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 # Load labels
-labels_csv = pd.read_csv("D:\dog image\data\labels.csv")
+labels_csv = pd.read_csv(".\data\labels.csv")
 labels = labels_csv["breed"].to_numpy()
 unique_breeds = np.unique(labels)
 
